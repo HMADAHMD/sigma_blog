22d96858ef4457b16552712388bc1250a9ec42e5
Hammad Ahmad <hammadja01@gmail.com>
2023-07-19 17:35:08 +0500

    started SigmaBlogging App

---
+++ app/models/article.rb
@@ -0,0 +1,8 @@
+class Article < ApplicationRecord
+    # this will validate that the title should not be empty
+    validates :title, presence: true
+    # this will validate that the description should not be empty
+    validates :description, presence: true, length: {minimum: 10, maximum: 400}
+    # validates the length of title should not be less then 3 and not more than 50
+    validates :title, length: {minimum: 3, maximum: 50}
+end
